fe7c4631f969dca36fb04027b61043979b2c94fc
Oleg-Korol <olegkorol89@mail.ru>
2022-11-05 11:41:55 +0300

    Update Ex_02_13.py

--- hw13/Ex_02_13.py
+++ hw13/Ex_02_13.py
@@ -2,11 +2,9 @@
 # Найти заказчиков, у которых нет ни одного заказа. Вывести имя заказчика и order_id.
 
 
-#У кого нет заказов не нашел, вывел всех у кого нет ship_region
 
-
-SELECT c.company_name,ship_region
+SELECT c.company_name
 fROM customers c
 left join orders o on c.customer_id = o.customer_id
-where ship_region is null
-group by c.company_name, ship_region
+where order_id is null
+group by c.company_name
