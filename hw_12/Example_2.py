3495a94b4f0192c81958c6f06de15fa30ef78ca6
Oleg-Korol <olegkorol89@mail.ru>
2022-11-05 10:48:56 +0300

    Update Example_2.py

--- hw_12/Example_2.py
+++ hw_12/Example_2.py
@@ -20,22 +20,22 @@ connection = create_connection()
 
 #создаем таблицу
 create_users_table =("""CREATE TABLE IF NOT EXISTS Employees(
-                 Employs_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 first_name  varchar(10),
-                 last_name   varchar(15),
-                 email       varchar(15),
-                 phone_number varchar(15),
-                 hire_date   varchar(10), 
-                 job_id        varchar(15),
+                 employe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 first_name  VARCHAR(10),
+                 last_name   VARCHAR(15),
+                 email       VARCHAR(15),
+                 phone_number VARCHAR(15),
+                 hire_date   VARCHAR(10), 
+                 job_id        INTEGER(15),
                  manager_id VARCHAR(3) ,
-                 department_id  int );""")
+                 department_id  INTEGER(4);""")
 execute_query(connection, create_users_table)
 
 #заполняем таблицу
 create_users =("""INSERT INTO Employees(first_name,last_name,email,phone_number,hire_date,job_id ,manager_id, department_id) 
-                VALUES('Ivan','Ivanov', 'qwqw@.com','232-43-42','01.02.2019',5,"YES","Bank"),
-                ('Mark','Petrov', 'qcxx@.com','252-53-62','06.06.2019',7,"NO","Bank"),
-                ('Alex','Sidorov', 'dfdffdw@.com','255-43-33','11.07.2022',9,"YES","Bank")""")
+                VALUES('Ivan','Ivanov', 'qwqw@.com','232-43-42','01.02.2019',5,"YES",312),
+                ('Mark','Petrov', 'qcxx@.com','252-53-62','06.06.2019',7,"NO",201),
+                ('Alex','Sidorov', 'dfdffdw@.com','255-43-33','11.07.2022',9,"YES",305)""")
 execute_query(connection,create_users)
 
 # выводим таблицу
